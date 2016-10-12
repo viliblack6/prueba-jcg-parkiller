845ace18cf2b943cb53a876a220c446ce803db7d
viliblack6 <troteats@gmail.com>
2016-10-12 12:57:53 -0500

    Cambios scripts

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,17 +4,29 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<title>Prueba Javier Cureño Guzmán</title>
+	<!--
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/main.css') }}">
+	-->
+	<link rel="stylesheet" href="http://prueba-jcg-parkiller.herokuapp.com/css/app.css">
+	<link rel="stylesheet" href="http://prueba-jcg-parkiller.herokuapp.com/css/main.css">
 </head>
 <body>
 	@yield('content')
+	<!--<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCHOQKjk1460S1OpbA1i12N8zs2--nTV1k&signed_in=true" async defer></script>-->
+	<!--http://prueba-jcg-parkiller.herokuapp.com/css/main.css
 	<script src="{{ asset('js/jquery.min.js') }}"></script>
 	<script src="{{ asset('js/bootstrap.min.js') }}"></script>
-	<!--<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCHOQKjk1460S1OpbA1i12N8zs2--nTV1k&signed_in=true" async defer></script>-->
 	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 	<script src="{{ asset('js/v3_epoly.js') }}"></script>
 	<script src="{{ asset('js/script.js') }}"></script>
 	<script src="{{ asset('js/urchin.js') }}"></script>
+	-->
+	<script src="http://prueba-jcg-parkiller.herokuapp.com/js/jquery.min.js"></script>
+	<script src="http://prueba-jcg-parkiller.herokuapp.com/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+	<script src="http://prueba-jcg-parkiller.herokuapp.com/js/v3_epoly.js"></script>
+	<script src="http://prueba-jcg-parkiller.herokuapp.com/js/script.js"></script>
+	<script src="http://prueba-jcg-parkiller.herokuapp.com/js/urchin.js"></script>
 </body>
 </html>
